e35161d093e49984175f2512f795faa653f4f2c4
Matias Gea <matias.gea@globant.com>
2014-09-09 09:56:53 -0300

    Now it's working. Modified license and versioning.

--- tasks/apimock.js
+++ tasks/apimock.js
@@ -15,7 +15,9 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('apimock', 'Grunt wrapper for API-Mock', function() {
+  grunt.registerTask('apimock', 'Grunt wrapper for API-Mock', function() {
+
+    var done = grunt.task.current.async();
 
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
@@ -32,7 +34,9 @@ module.exports = function(grunt) {
 
     apimockInstance.run();
 
-    console.log('Mock API server running at http://localhost:' + apimockConfiguration.options.port + ', serving ' + options.src);
+    grunt.log.write('Mock API server running at http://localhost:' + apimockConfiguration.options.port + ', serving ' + options.src);
+
+    done(true);
 
   });
 
